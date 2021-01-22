4490c547d0666e3696ee758de7ac16ea2534dec3
josevillacres <nuna.josepajaro99@gmail.com>
2021-01-21 22:54:30 -0500

    arreglo module.ts

--- SmartShop/src/app/app.module.ts
+++ SmartShop/src/app/app.module.ts
@@ -5,23 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
-<<<<<<< HEAD
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 import { ClientesListarProductosComponent } from './components/clientes-listar-productos/clientes-listar-productos.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesListarProductosComponent
-=======
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-@NgModule({
-  declarations: [
-    AppComponent,
+    ClientesListarProductosComponent,
     FooterComponent,
     HeaderComponent,
     HomeComponent
->>>>>>> 86af808b039ebf62d5c8dc45167fd770304996a8
   ],
   imports: [
     BrowserModule,
