1ea486752ae658f24d05fcd4e994f3564e64d702
frank16g <37024864+frank16g@users.noreply.github.com>
2021-01-24 01:18:12 -0500

    Revert "Merge branch 'master' of https://github.com/frank16g/gestion-pedidos"
    This reverts commit 2a17df1d5487babe106d43753a6f83474b0c93e4, reversing
    changes made to 48f0eab1cadd0f55156d313cabf56882e18ec68b.

--- SmartShop/src/app/auth/login/login-routing.module.ts
+++ SmartShop/src/app/auth/login/login-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login.component';
-import { HeaderComponent} from '../../components/header/header.component'
 
 const routes: Routes = [{ path: '', component: LoginComponent }];
 
